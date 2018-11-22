44112b60c831a6f85a591760ddba5a069b53ba90
Stephanie Rentfrow <stephanie.rentfrow@gmail.com>
2018-11-22 17:48:15 -0500

    Finished mobile menu functionality

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, HostListener } from '@angular/core';
+import { Component, OnInit, HostListener, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-navigation',
@@ -8,12 +8,25 @@ import { Component, OnInit, HostListener } from '@angular/core';
 export class NavigationComponent implements OnInit {
   menuOpen = false;
 
-  constructor() { }
+  constructor(private _elementRef: ElementRef) { }
+
+  @HostListener('document:click', ['$event.target'])
+  onclick(target) {
+    const insideMenu = this._elementRef.nativeElement.contains(target);
+    if(!insideMenu && this.menuOpen === true) {
+      this.menuOpen = false;
+    }
+  }
 
   ngOnInit() { }
 
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
-    console.log(this.menuOpen);
+  }
+
+  closeMenu() {
+    if (window.outerWidth < 768) {
+      this.menuOpen = false;
+    }
   }
 }
